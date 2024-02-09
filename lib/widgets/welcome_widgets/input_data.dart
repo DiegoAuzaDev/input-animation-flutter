c39a40cd537948610dda22ac54b3b3e8a0848847
auza0002 <auzajuan1@gmail.com>
2024-02-09 11:25:56 -0500

    validating user input

--- lib/widgets/welcome_widgets/input_data.dart
+++ lib/widgets/welcome_widgets/input_data.dart
@@ -8,11 +8,31 @@ class InputUser extends StatefulWidget {
 }
 
 class _InputUserState extends State<InputUser> {
-  int employerID = 0;
+  final String _invalidLengthMessage =
+      "Por favor ingrese un valor que tenga como minimo 6 digitos";
+  final String _invalidValueTypeMessage =
+      "Por favor solo ingrese valores numericos";
 
-  int employeeID = 0;
+  String employerID = "";
+
+  String employeeID = "";
 
   final _formKey = GlobalKey<FormState>();
+
+  String? validateUserInput(String? value) {
+    if (value == null || value.trim().isEmpty || value.trim().length < 4) {
+      return _invalidLengthMessage;
+    }
+    if (value.contains(",") ||
+        value.contains("-") ||
+        value.contains(" ") ||
+        value.contains(".")) {
+      return _invalidValueTypeMessage;
+    }
+    return null;
+  }
+
+  void authUserInput() {}
 
   @override
   Widget build(BuildContext context) {
@@ -56,36 +76,54 @@ class _InputUserState extends State<InputUser> {
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
+                    // ------- GET EMPLOYER ID -----
                     TextFormField(
+                      validator: (value) {
+                        validateUserInput(value);
+                      },
+                      onSaved: (value) {
+                        employerID = value!;
+                      },
                       keyboardType: TextInputType.number,
                       autocorrect: false,
                       maxLength: 20,
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurface),
                       decoration: InputDecoration(
                         labelText: "Identificador empresa",
-                        icon: Icon(Icons.work_rounded,
-                            color: Theme.of(context).colorScheme.primary),
+                        icon: Icon(
+                          Icons.work_rounded,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
                       ),
                     ),
+                    // ------- GET EMPLOYER ID -----
+                    //
+                    // ------- GET EMPLOYEE ID -----
                     TextFormField(
                       keyboardType: TextInputType.number,
                       autocorrect: false,
                       maxLength: 20,
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSurface),
                       decoration: InputDecoration(
                         labelText: "Mi identificador",
                         icon: Icon(Icons.person,
                             color: Theme.of(context).colorScheme.primary),
                       ),
                     ),
+                    // ------- GET EMPLOYEE ID -----
                     const SizedBox(
                       height: 30,
                     ),
+                    // ------- AUTH USER INPUT  -----
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
                           foregroundColor:
                               Theme.of(context).colorScheme.onPrimary,
                           backgroundColor:
                               Theme.of(context).colorScheme.primary),
-                      onPressed: () {},
+                      onPressed: authUserInput,
                       child: const Row(
                         mainAxisSize: MainAxisSize.max,
                         mainAxisAlignment: MainAxisAlignment.center,
@@ -98,7 +136,8 @@ class _InputUserState extends State<InputUser> {
                           ),
                         ],
                       ),
-                    )
+                    ),
+                    // ------- AUTH USER INPUT  -----
                   ],
                 ),
               )
