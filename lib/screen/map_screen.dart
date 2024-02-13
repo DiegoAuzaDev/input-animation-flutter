c6d12e4fee2af208415483a5dbce6776ba7a514c
auza0002 <auzajuan1@gmail.com>
2024-02-13 12:30:50 -0500

    Adding button animation

--- lib/screen/map_screen.dart
+++ lib/screen/map_screen.dart
@@ -22,6 +22,8 @@ class _MapScreenState extends State<MapScreen> {
     });
   }
 
+  double notificationDraggable = 0.25;
+
   @override
   void initState() {
     super.initState();
@@ -34,20 +36,18 @@ class _MapScreenState extends State<MapScreen> {
       body: Stack(
         children: [
           Positioned.fill(
-            bottom: MediaQuery.of(context).size.height * 0.2,
+            bottom: MediaQuery.of(context).size.height * 0.25,
             child: GoogleMap(
+              myLocationButtonEnabled: false,
+              myLocationEnabled: true,
+              rotateGesturesEnabled: true,
+              compassEnabled: true,
               mapType: MapType.satellite,
               zoomControlsEnabled: false,
               initialCameraPosition: CameraPosition(
                 target: userLatLng,
                 zoom: 14.4746,
               ),
-              markers: {
-                Marker(
-                  markerId: const MarkerId("USER_ID_UNIQUE"),
-                  position: LatLng(userLatLng.latitude, userLatLng.longitude),
-                )
-              },
             ),
           ),
           const Positioned(
@@ -57,118 +57,219 @@ class _MapScreenState extends State<MapScreen> {
           ),
           Positioned.fill(
             bottom: 0,
-            child: DraggableScrollableSheet(
-              expand: true,
-              initialChildSize: 0.25,
-              minChildSize: 0.25,
-              maxChildSize: 0.8,
-              builder: (context, scrollController) {
-                return Material(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                  color: Theme.of(context).colorScheme.background,
-                  child: Column(
+            child: NotificationListener<DraggableScrollableNotification>(
+              onNotification: (notification) {
+                setState(() {
+                  notificationDraggable = notificationDraggable =
+                      1 - ((notification.extent - 0.25) / (1 - 0.25));
+                });
+                print(notificationDraggable);
+                return true;
+              },
+              child: DraggableScrollableSheet(
+                expand: true,
+                initialChildSize: 0.25,
+                minChildSize: 0.25,
+                maxChildSize: 1,
+                builder: (context, scrollController) {
+                  return Stack(
+                    clipBehavior: Clip.none,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: 15,
-                          right: 15,
-                          top: 20,
-                          bottom: 10,
+                      Material(
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
                         ),
+                        color: Theme.of(context).colorScheme.background,
                         child: Column(
-                          mainAxisSize: MainAxisSize.min,
                           children: [
-                            Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Expanded(
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(bottom: 8),
-                                    child: Text(
-                                      "Ruta Florest Sur - Diego Auza",
-                                      maxLines: 2,
-                                      overflow: TextOverflow.fade,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleMedium!
-                                          .copyWith(
-                                              fontWeight: FontWeight.bold,
-                                              color: Theme.of(context)
-                                                  .colorScheme
-                                                  .onBackground),
-                                    ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                left: 15,
+                                right: 15,
+                                top: 20 + (30 * (1 - notificationDraggable)),
+                                bottom: 15,
+                              ),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Expanded(
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(bottom: 8),
+                                          child: Text(
+                                            "Ruta Florest Sur - Diego Auza T",
+                                            maxLines: 2,
+                                            overflow: TextOverflow.fade,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleMedium!
+                                                .copyWith(
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Theme.of(context)
+                                                        .colorScheme
+                                                        .onBackground),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ),
-                              ],
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "\u2022 Tiempo 6 Horas",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelSmall!
+                                            .copyWith(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .onBackground),
+                                      ),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Text(
+                                        "\u2022 Paradas 20",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelSmall!
+                                            .copyWith(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .onBackground),
+                                      ),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Text(
+                                        "\u2022 Distancia 15KM",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelSmall!
+                                            .copyWith(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .onBackground),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
-                            Row(
-                              children: [
-                                Text(
-                                  "\u2022 Tiempo 6 Horas",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelSmall!
-                                      .copyWith(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .onBackground),
-                                ),
-                                const SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  "\u2022 Paradas 20",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelSmall!
-                                      .copyWith(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .onBackground),
-                                ),
-                                const SizedBox(
-                                  width: 20,
-                                ),
-                                Text(
-                                  "\u2022 Distancia 15KM",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelSmall!
-                                      .copyWith(
-                                          color: Theme.of(context)
-                                              .colorScheme
-                                              .onBackground),
-                                ),
-                              ],
+                            Expanded(
+                              child: ListView.builder(
+                                controller: scrollController,
+                                itemCount: 20,
+                                itemBuilder: (context, index) {
+                                  return ListTile(
+                                    leading: const Icon(
+                                      Icons.location_on_rounded,
+                                    ),
+                                    title: Text("Tienda donde pedro #$index"),
+                                  );
+                                },
+                              ),
                             ),
                           ],
                         ),
                       ),
-                      Expanded(
-                        child: ListView.builder(
-                          controller: scrollController,
-                          itemCount: 20,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              leading: const Icon(
-                                Icons.location_on_rounded,
+                      Positioned(
+                        top: -40 + (150 * (1 - notificationDraggable)),
+                        right: 0,
+                        left: 0,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .primaryContainer,
+                                  foregroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .onPrimaryContainer),
+                              onPressed: () {},
+                              child: SizedBox(
+                                height: 50,
+                                width: MediaQuery.of(context).size.width * 0.8,
+                                child: const Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text("Tap para empezar tu ruta"),
+                                    Icon(
+                                        Icons.subdirectory_arrow_right_outlined)
+                                  ],
+                                ),
                               ),
-                              title: Text("Tienda donde pedro #$index"),
-                            );
-                          },
+                            ),
+                          ],
                         ),
-                      ),
+                      )
                     ],
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
+          )
         ],
       ),
     );
   }
 }
+
+
+
+
+  // Row(
+  //                       mainAxisAlignment: MainAxisAlignment.center,
+  //                       children: [
+  //                         ElevatedButton(
+  //                           style: ElevatedButton.styleFrom(
+  //                               backgroundColor: Theme.of(context)
+  //                                   .colorScheme
+  //                                   .primaryContainer,
+  //                               foregroundColor: Theme.of(context)
+  //                                   .colorScheme
+  //                                   .onPrimaryContainer),
+  //                           onPressed: () {},
+  //                           child: const Expanded(
+  //                             child: Row(
+  //                               mainAxisSize: MainAxisSize.max,
+  //                               children: [
+  //                                 Text("Empezar Ruta"),
+  //                                 Icon(Icons.subdirectory_arrow_right_sharp)
+  //                               ],
+  //                             ),
+  //                           ),
+  //                         ),
+  //                       ],
+  //                     ),
+
+
+    // ElevatedButton(
+    //                         style: ElevatedButton.styleFrom(
+    //                             backgroundColor: Theme.of(context)
+    //                                 .colorScheme
+    //                                 .primaryContainer,
+    //                             foregroundColor: Theme.of(context)
+    //                                 .colorScheme
+    //                                 .onPrimaryContainer),
+    //                         onPressed: () {},
+    //                         child: const Text("adsfasdfasdfas"),
+    //                       ),
+
+
+        // markers: {
+              //   Marker(
+              //     markerId: const MarkerId("USER_ID_UNIQUE"),
+              //     position: LatLng(userLatLng.latitude, userLatLng.longitude),
+              //   )
+              // },
